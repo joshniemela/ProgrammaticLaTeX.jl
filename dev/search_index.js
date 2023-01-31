f6c11c4f6798e4611da36f7c77343b3590a66c7a
Documenter.jl <documenter@juliadocs.github.io>
2023-01-31 18:37:21 +0000

    build based on 50c3c37

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProgrammaticLaTeX","category":"page"},{"location":"#ProgrammaticLaTeX","page":"Home","title":"ProgrammaticLaTeX","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProgrammaticLaTeX.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProgrammaticLaTeX]","category":"page"}]
+}
