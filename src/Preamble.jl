a72ff1ec3071ebacce121a22c8d47eca739b40a6
Joshua Niemelä <josh@jniemela.dk>
2023-02-01 02:58:10 +0100

    minimal viable product

--- src/Preamble.jl
+++ src/Preamble.jl
@@ -1,6 +1,6 @@
 module ProgrammaticLaTeX
 using Dates
-
+using Pipe
 # Stuff that isn't present in the acutal finished document, contains metadata other things
 abstract type AbstractDecl end
 
@@ -11,31 +11,39 @@ end
 DocumentClass(class::AbstractString) = DocumentClass(class, [""]) # If no settings are given
 
 struct Author <: AbstractDecl
-    name::Vector{AbstractString}
+    fields::Vector{AbstractString}
 end
 Author(name::AbstractString) = Author([name])
 
-struct Authors <: AbstractDecl
-    authors::Vector{Author}
-end
 
 struct Title <: AbstractDecl
     text::AbstractString
 end
 
 # Used if the builtin functions lack something
-struct Include <: AbstractDecl
-    path::AbstractString
-end
+#struct Include <: AbstractDecl
+#    path::AbstractString
+#end
 
 # \\ separate within author entry
 # \and to separate authors
 # \AND to separete authors more
 
-
-
-struct TOC <: AbstractDecl
+"""
+This collects all of the authors in the preamble and puts them into a list.
+"""
+function merge_authors(preamble)
+    authors = Vector{Author}()
+    notauthors =[]
+    for decl in preamble
+        decl isa Author ? push!(authors, decl) : push!(notauthors, decl)
+    end
+    
+    push!(notauthors, authors)
 end
+
+#struct TOC <: AbstractDecl
+#end NOT A DECLARATION!!!
 
 struct Document
     preamble
@@ -50,7 +58,7 @@ struct Document
             error("Date cannot have more than one definition.")
         end
 
-        new(preamble, content)
+        new(merge_authors(preamble), content)
     end
 end
 
@@ -58,23 +66,46 @@ function write_decl(docclass::DocumentClass)
     if isempty(docclass.options)
         "\\documentclass{$(docclass.class)}"
     else
-        "\\documentclass[$(join(d.settings, ','))]{$(docclass.class)}"
+        "\\documentclass[$(join(docclass.options, ','))]{$(docclass.class)}"
     end
+end
+
+#write_decl(::TOC) = "\\tableofcontents"
+
+write_decl(authors::Vector{Author}) = @pipe authors .|> 
+    join(_.fields, " \\\\ ") |>
+    join(_, " \\and ") |> 
+    "\\author{$_}"
+
+write_decl(date::Date) = "\\date{$date}"
+
+write_decl(title::Title) = "\\title{$(title.text)}"
+
+
+
+
+build_preamble(declarations) = @pipe declarations .|> write_decl |> join(_, "\n")
+
+
+function julia2latex(document::Document)
+    preamble = build_preamble(document.preamble)
+
+    content = "\\begin{document}\n"
+
+    if any(x -> x isa Title, document.preamble)
+        content *= "\\maketitle\n"
+    end
+
+    "$(preamble)\n$(content)\\end{document}"
 end
 
 
 doc = Document([
   DocumentClass("article"),
-  TOC(),
   Author("Joshua Niemelä"),
   Author(["Jakup Lützen", "Hold 3"]),
-  Date("2022-01-31")],
-
+  Date("2022-01-31"),
+  Title("Test 101")],
   []
 )
-
-function julia2latex(document::Document)
-    output = ""
-end
-
 end
