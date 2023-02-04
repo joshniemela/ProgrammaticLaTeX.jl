664da3caa0af57d1500e4de549fe01f66a558b30
Joshua Niemelä <josh@jniemela.dk>
2023-02-04 13:30:58 +0100

    removed preamble from module

--- src/ProgrammaticLaTeX.jl
+++ src/ProgrammaticLaTeX.jl
@@ -9,19 +9,18 @@ using Plots
 include("Preamble.jl")
 include("Body.jl")
 
-preamble = Preamble(
-  Author(["Josh", "Hold 5"]),
-  Author(["Jakup", "Hold 3"]),
-  Date("2022-01-31"),
-  Title("Test 101"),
-  default_pkgs...
-)
-
-
 function julia2latex(preamble::Preamble, body::Body; class=:article, kwargs...)
     preamble_str = build_preamble(preamble.preamble)
     body_str = interpret_item(body.content, numbered=false, depth=1)
-    join(["\\documentclass{$(class |> String)}", preamble_str, "\\begin{document}", body_str, "\\end{document}"], "\n")
+    #join(["\\documentclass{$(class |> String)}", preamble_str, "\\begin{document}", body_str, "\\end{document}"], "\n")
+    """
+    \\documentclass{$(class |> String)}
+    $preamble_str
+
+    \\begin{document}
+    $body_str
+    \\end{document}
+    """
 end
 
 function buildPDF(latex, name; run_command=`latexmk -lualatex`)
